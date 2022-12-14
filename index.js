f119305b36f60fd0ee1a24cc23fde180f51ef4b5
Udit Padhan <uditpadhan98@gmail.com>
2022-12-14 20:14:21 +0530

    final commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const endDate = "20 December 2022 11:20:00 PM"
+
+document.getElementById("end-date").innerText = endDate;
+const inputs = document.querySelectorAll("input")
+    
+function clock() {
+    const end = new Date(endDate)
+    const now = new Date()
+
+    const diff = (end - now) / 1000;
+
+    if (diff < 0) return;
+
+    // convert into days;
+    inputs[0].value = Math.floor(diff / 3600 / 24);
+
+    // convert into hour
+    inputs[1].value = Math.floor(diff / 3600) % 24;
+
+    // convert into minutes
+    inputs[2].value = Math.floor(diff / 60) % 60; 
+
+    // convert into seconds
+    inputs[3].value = Math.floor(diff) % 60;
+}
+
+
+clock()
+
+setInterval(()=>{
+        clock()
+    },1000)
